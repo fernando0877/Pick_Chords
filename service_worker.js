b594c4658361b55e3d7e6ee95f17c4cdd5d2010c
fernando0877 <fernando0877@gmail.com>
2025-05-12 17:21:26 -0300

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,4 @@
-// service-worker.js
+// service_worker.js
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open("v1").then((cache) =>
@@ -6,11 +6,14 @@ self.addEventListener("install", (e) => {
         "./",
         "./index.html",
         "./script.js",
+        "./style.css",
         "./manifest.json",
+        "./lista_musicas.json",
+        "./musicas_extraidas/musica1.txt", // exemplo
         "./icones/pick_512.png",
         "./icones/pick_192.png",
         "./icones/pick_48.png",
-        "./icones/pick_32.png", // adicione outros arquivos importantes
+        "./icones/pick_32.png"
       ])
     )
   );
@@ -20,4 +23,8 @@ self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
   );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(clients.claim());
 });
